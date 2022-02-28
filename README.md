ba20938e9ed3d0451d0568524779ee87f685db15
Gabby Hernandez <gabbyhdezz697@gmail.com>
2022-02-27 23:06:55 -0500

    update to scheduler

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Work-day-scheduler
+
+For this assignment, our goal was to create a Work day scheduler that has the current tiume and date, the hours of the day. The scheduler also has to be interactive and be able to have text and save the information to the local storage.
+
+The programs that were used were: HTML, CSS and JS. We also needed to use JQuery and Moment.JS
+
+For this week we were given starter code for the HTML and CSS. We had to start the JS ourselves.
+
+Steps:
+
+1. First we had to finish the HTML file that we were given. On that file we had to create the columns and rows where our information for each hour of the day was contained. WE also had to make sure that all of our files were linked.
+
+2. After that the CSS file needed to be updated to match any of the terms, classes or id's that we used in the HTML so all the information could match up.
+
+3. After the HTML and CSS were up standard to the Acceptance Criteria, the next step was to work on the JS.
+
+4. In the JS, we used Moment.js to include the date and time on the website. The save button had to be connected to the local storage so the text could be save.
+
+![Scheduler](./Assets/05-third-party-apis-homework-demo.gif)
+
+
+GitHub URL:
+URL: 
---
+++ assets/script.js
@@ -0,0 +1,53 @@
+$("#currentDay").text(moment().format('dddd MMMM Do, YYYY'));
+
+
+function scheduleHour() {
+   
+    var currentHour = moment().hour(); 
+
+
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function() {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+});
+
+scheduleHour(); 
